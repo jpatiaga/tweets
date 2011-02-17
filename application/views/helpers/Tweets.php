8d6f790cecb7c27c5811a467b19be18e42b4605d
JP <Juan Pablo@.(none)>
2011-02-17 11:15:03 -0500

    twitter ajax

--- application/views/helpers/Tweets.php
+++ application/views/helpers/Tweets.php
@@ -10,21 +10,6 @@ class Zend_View_Helper_Tweets extends Zend_View_Helper_Abstract
 						});
 						</script>';
 	
-        $auth = Zend_Auth::getInstance();
-        if ($auth->hasIdentity()) {
-            $username = $auth->getIdentity()->username;
-            $logoutUrl = $this->view->url(array('controller'=>'auth',
-                'action'=>'logout'), null, true);
-            return 'Welcome ' . $username .  '. <a href="'.$logoutUrl.'">Logout</a>';
-        } 
-
-        $request = Zend_Controller_Front::getInstance()->getRequest();
-        $controller = $request->getControllerName();
-        $action = $request->getActionName();
-        if($controller == 'auth' && $action == 'index') {
-            return '';
-        }
-        $loginUrl = $this->view->url(array('controller'=>'auth', 'action'=>'index'));
-        return '<a href="'.$loginUrl.'">Login</a>';
+        return $html;
     }
 }
