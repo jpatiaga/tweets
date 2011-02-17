cee3626b7302a328fa5b4870ce9d6c873d3c1bdf
JP <Juan Pablo@.(none)>
2011-02-17 11:25:24 -0500

    twitter ajax

--- application/views/helpers/Tweets.php
+++ application/views/helpers/Tweets.php
@@ -5,8 +5,12 @@ class Zend_View_Helper_Tweets extends Zend_View_Helper_Abstract
     {
 				$html = '<div id="tweets"></div>
 						<script type="text/javascript">
-						$(document).ready(function() {
+						function loadTweets() {
 								$("#tweets").load("/index.php/post/list/format/html?userid=' . $userid . '");
+								setTimeout("loadTweets()", 60000);
+						}
+						$(document).ready(function() {
+								loadTweets();
 						});
 						</script>';
 	
